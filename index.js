49b674fee97979ef9ede2c463ae9327943f56ca6
Tchukamira <vodocunha@gmail.com>
2023-07-04 18:30:14 -0300

    CV Site

---
+++ index.js
@@ -0,0 +1,27 @@
+const hamburger = document.querySelector(".hamburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const mobileNav = document.querySelector(".mobileNav")
+
+hamburger.addEventListener("click", () => {
+    bar1.classList.toggle("animateBar1")
+    bar2.classList.toggle("animateBar2")
+    bar3.classList.toggle("animateBar3")
+    mobileNav.classList.toggle("openDrawer")
+})
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
